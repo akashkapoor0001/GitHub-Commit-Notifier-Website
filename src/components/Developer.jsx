fd48baa7dfc6421f0560685debfba7fd1fbdac8c
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-07-11 20:09:24 +0530

    Added about the Developer.

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -7,16 +7,22 @@ const Testimonials = () => (
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-      <h2 className={styles.heading2}>
-        What People are <br className="sm:block hidden" /> saying about us
-      </h2>
+      <div className="w-full md:mb-6 mb-4"> {/* Added div to wrap the heading */}
+        <h2 className={`${styles.heading2} mt-4`}>
+          Meet the {" "} 
+          <span className="text-gradient">Developer</span>{" "}
+        </h2>
+      </div>
       <div className="w-full md:mt-0 mt-6">
         <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Everything you need to accept card payments and grow your business
-          anywhere on the planet.
+        Crafted with passion and precision by a coding enthusiast who loves turning ideas into reality. Dive into the seamless functionality and elegant design brought to you by the creative mind behind this project. This project {''}
+        <span className="text-gradient">GitHub Commit Notifier</span>{" "} is designed to make it easier to keep track of your collaborators' commits in real-time. {" "}
+        <span className="flex-1 font-poppins font-semibold ss:text-[25px] text-[15px] text-gradient">~Akash Kapoor</span>
         </p>
       </div>
     </div>
+
+
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
       {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
