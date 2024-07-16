c38a689a42f18b52cca729e2fe4a88338958acee
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-07-17 01:17:47 +0530

    Updated the get started button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => (
   <button 
     type="button" 
     className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
-    onClick={() => window.open("https://guided-tour.vercel.app/", "_blank")}
+    // onClick={() => window.open("https://guided-tour.vercel.app/", "_blank")}
   >
     Get Started
   </button>
