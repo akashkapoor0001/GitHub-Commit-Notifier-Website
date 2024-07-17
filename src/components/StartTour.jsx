f770c80d560aaec038a4ad541218b26a91e7c3dc
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-07-17 11:03:15 +0530

    Changed the position of the Start Tour Button.

--- src/components/StartTour.jsx
+++ src/components/StartTour.jsx
@@ -4,7 +4,7 @@ import "../index.css";
 const Button = ({ styles }) => (
   <button 
     type="button" 
-    className={`py-4 px-6 font-poppins font-medium text-[18px] text-gradient rounded-[10px] outline-none border-2 border-solid border-#3a6b98 hover:bg-blue-gradient hover:text-black transition-all duration-300 ${styles}`}
+    className={`py-4 px-6  font-poppins font-medium text-[18px] text-gradient rounded-[10px] outline-none border-2 border-solid border-#3a6b98 hover:bg-blue-gradient hover:text-black transition-all duration-300 -mt-6 ${styles}`}
     onClick={() => window.open("https://guided-tour.vercel.app/", "_blank")}
   >
     Start Tour
