6c4ba60001891a892867d6f962841de4e710a264
AKASH KAPOOR <akashkapoor976@gmail.com>
2024-07-21 02:14:59 +0530

    Removed the unnecessary files.

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,61 +1,37 @@
 import Extension from "./Extension.png"
 import commit from "./commit.svg"
-import binance from "./binance.png";
-import card from "./card.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
 import github from "./github.png";
 import details from "./details.png";
 import circle from "./circle.png";
 import quotes from "./quotes.svg";
-import robot from "./robot.png";
 import Rocket from "./Rocket.svg";
 import warning from "./warning.svg";
 import Bell from "./Bell.svg";
 import shield from "./Shield.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import Chrome from "./Chrome.png"
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import Portfolio from "./Portfolio.png"
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 
 export {
   Extension,
-  binance,
-  card,
-  coinbase,
-  dropbox,
+  commit,
   github,
   details,
   circle,
-  commit,
   quotes,
-  robot,
   Rocket,
+  warning,
   Bell,
   shield,
-  warning,
   menu,
   close,
-  google,
   Chrome,
-  apple,
   arrowUp,
-  discount,
   Portfolio,
   instagram,
   linkedin,
-  twitter,
-  people01,
-  people02,
-  people03,
 };
